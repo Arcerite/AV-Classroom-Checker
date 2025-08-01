7b6abe185a43d40dde8a12fc257cf8905428e123
Caleb Peters <122840165+Arcerite@users.noreply.github.com>
2025-08-01 11:57:30 -0400

    Updated Python Version
    Streamlined the code and removed the pdf's

--- Python-Version/Classroom_Checker.py
+++ Python-Version/Classroom_Checker.py
@@ -3,9 +3,9 @@ Description: The following Python script will automate the classroom checks and 
 
 Author: Caleb Peters/Arcerite
 
-Version: 1.5 : Python 3.13.1
+Version: 2.0 Python : Python 3.13.1
 
-Date 5/30/25
+Date 8/1/25
 """
 
 import os
@@ -78,7 +78,7 @@ def audio_check() -> bool:
         return False
 
 
-def camera_check() -> bool:
+def camera_check(mode:int=0) -> bool:
     """
     Open the camera app and prompt the user to verify functionality.
 
@@ -88,8 +88,12 @@ def camera_check() -> bool:
     clear()
     print("Say Cheese! Camera test!")
     try:
-        os.system("start microsoft.windows.camera:")
-        return prompter("Is the camera working?")
+        if mode == 0:
+            os.system("start microsoft.windows.camera:")
+            return prompter("Is the camera working?")
+        else:
+            print("In the top right corner of the camera app shoud be an option to swap cameras, please do so. \nCheck to see if Elmo is an option, it may show a white screen, give it a few seconds")
+            return prompter("Is the doc cam working?")
     except Exception as e:
         print(f"Error with camera: {e}")
         input("Press enter to continue")
@@ -153,45 +157,31 @@ def troubleshoot(ac: bool, ec: bool, cc: bool, oc: bool) -> None:
         cc (bool): Camera check result.
         oc (bool): MFA Duo override check result.
     """
-    def open_pdf(filename: str) -> None:
-        base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-        file_path = os.path.join(base_path, filename)
-        os.startfile(file_path)
+    if prompter("Would you like help troubleshooting?"):
 
-    if not cc and prompter("Do you want to open a PDF to help troubleshoot the camera?"):
-        open_pdf("Camera.pdf")
-        cc = camera_check()
+        if not cc:
+            print("***CAMERA HELP***\n\n\tCheck to see the camera is plugged into the computer, try replugging it in to its current port\n\nIf that didnt work try plugging it in to a different port. If this works you may need a USB hub for everything to be plugged in at once\n\nIf that did not work or if the camera is broken you may have to replace it")
+            prompter("Are you ready to continue?")
+            cc = camera_check()
 
-    if not ac and prompter("Do you want to open a PDF to help troubleshoot the sound?"):
-        open_pdf("Audio.pdf")
-        ac = audio_check()
+        if not ac:
+            print("***AUDIO HELP***\n\n\tTry changing the audio source, most of the time it should be on extron scaler however some situations may require it to be on speakers or realtek audio instead\n\nOpen the instructor station and check audio inputs, this would be on the right side. Check to see if anything is plugged into port 4. If so trace this cord and make sure its plugged into the computer\n\nTry restarting the system power, if issues still persist contact pro staff")
+            prompter("Are you ready to continue?")
+            ac = audio_check()
 
-    if not ec and prompter("Do you want to open a PDF to help troubleshoot the doc cam?"):
-        open_pdf("Elmo.pdf")
-        ec = elmo_check()
+        if not ec:
+            print("***DOC CAM HELP***/n/n/tCheck the model number of the doc cam, If it is a basic TT-12 with no letters (typically I or id) afterwards, then the doc cam is too old to support UVC and therefore wont work with doc cam usb\n\nIf the doc cam can support the UVC setting it has to be enabled. To do this, turn on the doc cam and click the settings/menu button. Find prefferences and look for USB options, Under this setting click UVC, the doc cam will reboot\n\n If the doc cam still doesn't work, try unplugging and replugging in the USB cord to the computer")
+            prompter("Are you ready to continue?")
+            ec = camera_check(1)
 
     results(ac, ec, cc, oc)
 
 
-def elmo_check() -> bool:
-    """
-    Open the Elmo (doc cam) website and prompt for confirmation.
-
-    Returns:
-        bool: True if the Elmo/doc cam is working, False otherwise.
-    """
-    clear()
-    print("Testing doc cam USB...")
-    print("A website will open. Allow access if prompted.")
-    print("You may need to close a pop-up box.")
-    webbrowser.open("https://imagemate-c.com/")
-    return prompter("Is the doc cam working?")
-
 
 if __name__ == "__main__":
     intro()
-    audio_result = audio_check()
     camera_result = camera_check()
-    elmo_result = elmo_check()
+    elmo_result = camera_check(1)
     duo_result = mfa_override_test()
+    audio_result = audio_check()
     troubleshoot(audio_result, elmo_result, camera_result, duo_result)
